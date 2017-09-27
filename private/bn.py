23892a3184c288000d4e93477d88edf63898f01b
unknown <1174534295@qq.com>
2017-09-27 15:08:20 +0800

    adapt bpnet

--- private/bn.py
+++ private/bn.py
@@ -1,4 +1,115 @@
 import numpy as np 
+
+# adapt:
+class MemoryManager:
+	mark_int=1
+	mark_float=2
+	mark_double=3
+	remark={mark_int:np.int32,mark_float:np.float32,mark_double:np.float64}
+	mark_size={mark_int:4,mark_float:4,mark_double:8}
+	current_memory=0
+	total_memory=1024**3
+	bound_memory=3*1024**3/4
+	@staticmethod
+	def mem_used(*args):
+		total=0
+		for dt in args:
+			if dt is None:
+				continue
+			mark=MemoryManager.get_mark(dt.dtype)
+			unit_sz=mark_size[mark]
+			total+=dt.size*unit_sz
+		return total
+	@staticmethod
+	def total_used(net):
+		total=0
+		while net is not None:
+			total+=net.memused()
+			net=net.next
+		return total
+	@staticmethod
+	def need_save():
+		return current_memory>bound_memory
+	@staticmethod
+	def save(net):
+		tnet=net
+		while tnet.prev is not None:tnet=tnet.prev;
+		MemoryManager.current_memory=MemoryManager.total_used(tnet)
+		while net is not None and MemoryManager.need_save():
+			m=net.memused()
+			net.memsave()
+			current_memory-=m 
+			net=net.prev
+	@staticmethod
+	def get_mark(dtype):
+		for mark in MemoryManager.remark:
+			if MemoryManager.remark[mark]==dtype:
+				return mark
+		raise Exception("not such dtype in mark: "+str(dtype))
+	@staticmethod
+	def npsave(dt_wt,*args):
+		l=len(args)
+		dt_wt.puti(l);
+		out=[]
+		for i in xrange(l):
+			dt=args[i]
+			if dt is None:
+				dt_wt.puti(0)
+				continue
+			li=len(dt.shape)
+			dt_wt.puti(li)
+			dt_wt.putis(dt.shape)
+			if dt.dtype==np.float32:
+				puts=dt_wt.putfs
+				mark=MemoryManager.mark_float
+			elif dt.dtype==np.float64:
+				puts=dt_wt.putds
+				mark=MemoryManager.mark_double
+			elif dt.dtype==np.int32:
+				puts=dt_wt.putis
+				mark=MemoryManager.mark_int
+			else:
+				raise Exception("error save numpy dtype: "+str(dt.dtype)+" for index "+str(i))
+			dt_wt.puti(mark)
+			puts(dt.reshape(dt.size))
+			out.append(None)
+		return out
+	@staticmethod
+	def nprestore(dt_rd):
+		l=dt_rd.geti()
+		out=[]
+		for i in xrange(l):
+			li=dt_rd.geti()
+			if li == 0:
+				out.append(None)
+				continue
+			shape=range(li)
+			dt_rd.getis(li,shape)
+			mark=dt_rd.geti()
+			dtype=MemoryManager.remark[mark]
+			dt=np.empty(shape,dtype=mark)
+			if dtype==np.float32:
+				gets=dt_wt.getfs
+			elif dtype==np.float64:
+				gets=dt_wt.getds
+			elif dtype==np.int32:
+				gets=dt_wt.getis
+			else:
+				raise Exception("error restore numpy dtype: "+str(dt.dtype)+" for index "+str(i))
+			gets(dt.size,dt.reshape(dt.size))
+			out.append(dt)
+		return out
+class LinkNet(object):
+	def memsave(self):
+		pass
+	def memrestore(self):
+		pass
+	def memused(self):
+		return 0
+	def checkmem(self):
+		if self.memused()==0:
+			self.memrestore()
+#adapt done
 class BatchNormalNet(LinkNet):
 	@staticmethod
 	def get_title(dt_rd,prev):
